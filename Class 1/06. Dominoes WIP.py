76856cf618bc53dccbba43be112f9760b6e5ac57
Valeria Mitchell <57600998+valemescudero@users.noreply.github.com>
2020-10-21 20:57:20 -0300

    Update 06. Dominoes WIP.py

--- Class 1/06. Dominoes WIP.py
+++ Class 1/06. Dominoes WIP.py
@@ -5,3 +5,12 @@ for i in range(maxNum):
   fichas += (i + 1)
 
 print(fichas)
+
+def game():
+  max_num = int(input())
+  x = 0
+  for i in range(max_num+1):
+    for j in range(x,max_num+1):
+      print(i, "-", j)
+    x = x+1
+game()
