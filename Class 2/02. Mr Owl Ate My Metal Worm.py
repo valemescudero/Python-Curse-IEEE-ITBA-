706b86ea632467ff490649e1c5445deb94fc365c
Valeria Mitchell <57600998+valemescudero@users.noreply.github.com>
2020-10-25 20:32:41 -0300

    Update 02. Mr Owl Ate My Metal Worm.py

--- Class 2/02. Mr Owl Ate My Metal Worm.py
+++ Class 2/02. Mr Owl Ate My Metal Worm.py
@@ -1,11 +1,17 @@
+#  Definir una función que detecte si una palabra es un palíndromo y devuelve True o False.
+#  Modificar la función para ignorar espacios, signos de puntuación, y que no haga distinción entre mayúsculas y minúsculas (pueden usar str.lower()). Pueden usar el nombre del desafío como un palindromo de ejemplo.
+
 def pal(str):
-  MARKS = set("ºª\!\"@·#$~%€&¬/()='?¡¿`^[+*]´¨{},;.:-_<>áéíóúâêîôûýàèìòù")
+  MARKS = set("ºª\!\"@·#$~%€&¬/()='?¡¿`^[+*]´¨{},;.:-_<>")
+  ACCENTS = {"á":"a","é":"e","í":"i","ó":"o","ú":"u","â":"a","ê":"e","î":"i","ô":"o","û":"u","ý":"y","à":"a","è":"e","ì":"i","ò":"o","ù":"u"}
 
   str = str.lower()
   str = str.replace(' ', '')
   for c in str:
     if c in MARKS:
       str = str.replace(c, '')
+    if c in ACCENTS:
+      str = str.replace(c, ACCENTS[c])
 
   new_str = ''
   for i in range (len(str)):
